ad0b59998ccc46411fb703e1e7aa1dc5bd506a0f
jvpgjava <jjdevv05@gmail.com>
2025-10-17 22:24:27 -0300

    feat:: add comentario

--- src/main/kotlin/com/jjnex/musicbot/MusicBotApplication.kt
+++ src/main/kotlin/com/jjnex/musicbot/MusicBotApplication.kt
@@ -6,6 +6,7 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class MusicBotApplication
 
+//classe pra rodar aplicacao principal erertere
 fun main(args: Array<String>) {
     runApplication<MusicBotApplication>(*args)
 }
